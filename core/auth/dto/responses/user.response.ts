4d67a984865b3fdb8920701a29944dbd45958bb4
Yo <franjomor@gmail.com>
2024-05-27 14:51:27 +0200

    feat: user dto response

---
+++ core/auth/dto/responses/user.response.ts
@@ -0,0 +1,20 @@
+import { DeepReadonly } from 'ts-essentials'
+import { User } from '../../domain/model/user.entity'
+
+type UserResponse = DeepReadonly<{
+    email: string
+    id: string
+    username: string
+    password: string
+}>
+
+const UserResponse = {
+    fromModel: (user: User): UserResponse => ({
+        email: user.emailStr,
+        id: user.id,
+        username: user.username,
+        password: user.password,
+    })
+}
+
+export { UserResponse}
