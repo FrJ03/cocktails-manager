306e307f92988c4b378517fd41858a0a83b7c5a8
Yo <franjomor@gmail.com>
2024-05-31 18:11:08 +0200

    fix: user type import

--- core/auth/infrastructure/persistence/user.data-mapper.ts
+++ core/auth/infrastructure/persistence/user.data-mapper.ts
@@ -1,10 +1,10 @@
 import { User } from "../../domain/model/user.entity";
 import { Email } from "../../domain/model/value-objects/email";
-import { UserRawData } from "./user.type";
+import { UserType } from "./user.type";
 import { FlattenMaps, ObjectId } from 'mongoose';
 
 const UserDataMapper = {
-    toModel: (user: FlattenMaps<UserRawData>): User => 
+    toModel: (user: FlattenMaps<UserType>): User => 
         new User(
             user._id.toString(),
             user.username,
