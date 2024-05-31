447f4158f899c10001464daca3cbb3371110a4d5
Yo <franjomor@gmail.com>
2024-05-31 18:07:31 +0200

    feat: authentication form body components

---
+++ app/app/auth/components/auth-form-body.tsx
@@ -0,0 +1,52 @@
+'use client'
+import React from "react";
+import { CardBody } from "@nextui-org/card";
+import { Input } from "@nextui-org/input";
+import { ClosedEye } from "./closed-eye";
+import { OpenedEye } from "./opened-eye";
+
+interface AuthFormBodyProperties{
+    email: string,
+    onChangeEmail: (newEmail: string) => void,
+    password: string,
+    onChangePassword: (newPassword: string) => void
+}
+
+export default function AuthFormBody(properties: AuthFormBodyProperties){
+    const [isVisible, setIsVisible] = React.useState(false);
+
+   const toggleVisibility = () => setIsVisible(!isVisible);
+    return (
+        <>
+            <CardBody className="flex-col content-center w-full">
+                <Input
+                    fullWidth={true}
+                    type="email"
+                    label="Email"
+                    placeholder="Enter your email"
+                    value={properties.email}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => properties.onChangeEmail(event.target.value)}
+                />
+                <Input
+                    fullWidth={true}
+                    label="Password"
+                    variant="bordered"
+                    value={properties.password}
+                    placeholder="Enter your password"
+                    endContent={
+                        <button className="focus:outline-none" type="button" onClick={toggleVisibility}>
+                        {isVisible ? (
+                            <ClosedEye className="text-2xl text-default-400 pointer-events-none" />
+                        ) : (
+                            <OpenedEye className="text-2xl text-default-400 pointer-events-none" />
+                        )}
+                        </button>
+                    }
+                    type={isVisible ? "text" : "password"}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => properties.onChangePassword(event.target.value)}
+                    />
+                
+            </CardBody>
+        </>
+    )
+}
