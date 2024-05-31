01e2342b4fa5928c44e53b84710aa41f5b591ea8
Yo <franjomor@gmail.com>
2024-05-31 20:26:32 +0200

    fix: api login

--- core/auth/infrastructure/api/login.api.ts
+++ core/auth/infrastructure/api/login.api.ts
@@ -5,17 +5,24 @@ import { LoginUserResponse } from '../../dto/responses/login-user.response'
 const LoginRouter = express.Router()
 
 LoginRouter.post('/', async (request: Request, response: Response) => {
-    if(!request.body.email || !request.body.password){
+    if(
+        !request.body.email ||
+        request.body.email == '' ||
+        !request.body.password ||
+        request.body.password == ''
+    ){
         response.sendStatus(400)
     }
-    const userRequest = LoginUserRequest.with({
-        email: request.body.email,
-        password: request.body.password
-    })
-
-    const userResponse: LoginUserResponse = await container.loginUser.with(userRequest)
-
-    response.status(userResponse.code || 200).send(userResponse)
+    else{
+        const userRequest = LoginUserRequest.with({
+            email: request.body.email,
+            password: request.body.password
+        })
+    
+        const userResponse: LoginUserResponse = await container.loginUser.with(userRequest)
+    
+        response.status(userResponse.code || 200).send(userResponse)
+    }
 })
 
 export { LoginRouter }
