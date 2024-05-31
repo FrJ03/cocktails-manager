5a5f42b2b43a10993dae3db4dd98fe05add05f63
Yo <franjomor@gmail.com>
2024-05-31 18:09:04 +0200

    feat: authentication log service

---
+++ app/app/auth/services/auth.service.ts
@@ -0,0 +1,43 @@
+import axios from "axios"
+
+interface AuthData{
+    user: {
+        email: string | undefined
+        username: string | undefined
+    }
+    token: string | undefined
+    code: number
+}
+
+const baseUrl: string = 'http://localhost:4000/api/login'
+
+const auth = async (email: string, password: string): Promise<AuthData> => {
+    try {
+        const response = await axios.post(`${baseUrl}`, {
+            email: email,
+            password: password
+        })
+    
+        return {
+            user: {
+                email: response.data.email,
+                username: response.data.username
+            },
+            token: response.data.token,
+            code: response.data.code
+
+        } as unknown as AuthData
+    } catch (error: any) {
+        return {
+            user: {
+                email: undefined,
+                password: undefined
+            },
+            token: undefined,
+            code: error.code
+
+        } as unknown as AuthData
+    }
+}
+
+export { auth }
