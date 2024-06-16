9d1aed9b618ac3495e7e030aee861b627cb3fd4f
Yo <franjomor@gmail.com>
2024-06-16 12:05:46 +0200

    feat: add cocktail page

---
+++ app/src/add-cocktail/page.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import Menu from "../commons/components/menu"
+import AddCocktailForm from "./components/add-cocktail-form"
+import * as AddCocktailService from './services/add-cocktail.service'
+
+export default function AddCocktail(){
+    const navigate = useNavigate()
+    const [token, setToken] = useState('')
+    
+    useEffect(() => {
+        const loggedUserJSON = window.localStorage.getItem('loggedUser')
+        if (loggedUserJSON) {
+            const userLogged = JSON.parse(loggedUserJSON)
+            setToken(userLogged.token)
+        }
+        else{
+            navigate('/auth')
+        }
+    }, [navigate])
+
+    const onSubmit = async (name: string, image: string) => {
+        await AddCocktailService.create(name, image, token)
+    }
+
+    return (
+        <>
+            <Menu/>
+            <AddCocktailForm onSubmit={onSubmit}/>
+        </>
+    )
+}
