9583d788b2143c2eb3fc0509863e90aebb143f05
Yo <franjomor@gmail.com>
2024-07-08 18:32:59 +0200

    feat: cocktail details as modal

--- app/src/cocktails/components/cocktail-card.tsx
+++ app/src/cocktails/components/cocktail-card.tsx
@@ -1,6 +1,10 @@
 import { Card, CardBody, CardFooter, CardHeader } from '@nextui-org/card'
 import { Image } from '@nextui-org/image'
+import { useState } from 'react'
 import { AiFillDelete } from 'react-icons/ai'
+import CocktailDetails from './cocktail-details'
+import Modal from 'react-modal'
+import { useDisclosure } from '@nextui-org/react'
 
 interface CocktailCardProperties{
     cocktail: {
@@ -12,25 +16,32 @@ interface CocktailCardProperties{
 }
 
 export default function CocktailCard({cocktail, deleteCocktail}: CocktailCardProperties){
+	const [isOpen, setIsOpen] = useState(false)
+
     return (
-        <Card className='size-40 rounded outline outline-main-color outline-offset-2 hover:outline-4' isPressable disableAnimation isHoverable>
-          <CardHeader className='fix justify-end'>
-            <div className='bg-red-500 p-1 rounded-md'>
-              <AiFillDelete color='white' onClick={() => deleteCocktail(cocktail.id)}/>
-            </div>
-          </CardHeader>
-          <CardBody className="place-items-center overflow-visible p-0">
-            <Image
-              shadow="sm"
-              radius="lg"
-              alt={cocktail.name}
-              className="size-full object-cover max-h-20"
-              src={cocktail.image}
-            />
-          </CardBody>
-          <CardFooter className="text-small justify-between">
-            <b>{cocktail.name}</b>
-          </CardFooter>
-        </Card>
-    )
+		<>
+			<Card onClick={() => setIsOpen(true)} className='size-40 rounded outline outline-main-color outline-2 hover:outline-4' isPressable disableAnimation isHoverable>
+				<CardHeader className='fix justify-end'>
+					<div className='bg-red-500 p-1 rounded-md'>
+					<AiFillDelete color='white' onClick={() => deleteCocktail(cocktail.id)}/>
+					</div>
+				</CardHeader>
+				<CardBody className="place-items-center overflow-visible p-0">
+					<Image
+					shadow="sm"
+					radius="lg"
+					alt={cocktail.name}
+					className="size-full object-cover max-h-20"
+					src={cocktail.image}
+					/>
+				</CardBody>
+				<CardFooter className="text-small justify-between">
+					<b>{cocktail.name}</b>
+				</CardFooter>
+			</Card>
+			<Modal isOpen={isOpen} onRequestClose={() => setIsOpen(false)}>
+				<CocktailDetails cocktail={cocktail} onClose={() => setIsOpen(false)}/>
+			</Modal>
+		</>
+	)
 }
